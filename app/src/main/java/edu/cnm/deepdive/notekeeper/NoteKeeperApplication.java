83d88e1f5162a623afb2459b09fc903db5cface7
nmitchell313 <nmitchell11@cnm.edu>
2021-11-04 15:10:08 -0600

    started dialog fragment.

--- app/src/main/java/edu/cnm/deepdive/notekeeper/NoteKeeperApplication.java
+++ app/src/main/java/edu/cnm/deepdive/notekeeper/NoteKeeperApplication.java
@@ -8,8 +8,12 @@ public class NoteKeeperApplication extends Application {
 
   public NoteKeeperApplication() {
     super();
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
     Stetho.initializeWithDefaults(this);
     NoteKeeperDatabase.setContext(this);
   }
-
 }
